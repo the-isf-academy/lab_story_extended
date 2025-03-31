32c0a7f11cced6583e11bcf44e8408427afa4e42
emma <eqbrown@isf.edu.hk>
2025-03-31 08:23:34 +0800

    add a few more lines to game

--- game.py
+++ game.py
@@ -3,3 +3,10 @@ from story_setup import main_story
 
 view = View()
 
+view.start_game(main_story)
+
+
+while main_story.is_running() == True:
+    chosen_node = view.menu("What would you want to do", main_story.get_current_children())
+   
+
